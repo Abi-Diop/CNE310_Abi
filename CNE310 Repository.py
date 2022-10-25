9085b1a74b5b8b895a774e12b2cfcf51791b15c8
Abi-Diop <116244914+Abi-Diop@users.noreply.github.com>
2022-10-24 19:33:03 -0700

    Add files via upload

---
+++ CNE310 Repository.py
@@ -0,0 +1,14 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+words = ["aardvark","cookies","asterisk","cowabunga","gryphon","angular","cringe"]
+i = 0
+c_counter = 0
+while i < len(words):
+    if words[i][0] == "c":
+        print(words[i])
+        c_counter = c_counter + 1
+    i += 1
+    
+
